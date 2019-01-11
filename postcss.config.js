b59455275ff6a0930303e309b1a40b131f114db7
Scott Watermasysk <scottwater@gmail.com>
2019-01-11 05:05:52 -0500

    minimize CSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss")("./_includes/tailwind.js"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    require('cssnano')({
+      preset:'default'
+    }),
   ]
 };
