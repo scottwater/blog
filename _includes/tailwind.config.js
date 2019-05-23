8f67e400f2594bdaa57fe7cb790af8ee75532e5d
Scott Watermasysk <scottwater@gmail.com>
2019-05-23 11:26:33 -0400

    tailwind 1.0

---
+++ _includes/tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  theme: {
+    maxWidth: { //based on 0.7
+      'xs': '20rem',
+      'sm': '30rem',
+      'md': '40rem',
+      'lg': '50rem',
+      'xl': '60rem',
+      '2xl': '70rem',
+      '3xl': '80rem',
+      '4xl': '90rem',
+      '5xl': '100rem',
+      'full': '100%',
+    },
+    fontFamily: {
+      'headlines' : [
+        'Rubik', 
+        'Constantia',
+        'Lucida Bright',
+        'Lucidabright',
+        'Lucida Serif',
+        'Lucida',
+        'DejaVu Serif',
+        'Bitstream Vera Serif',
+        'Liberation Serif',
+        'Georgia',
+        'serif',
+      ],
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
--- _pages/archive.md
+++ _pages/archive.md
@@ -6,7 +6,7 @@ link_title: Posts
 <div>
   <ul id="archive" class="list-reset">
   {%- for post in site.categories.blog -%}
-    <li class="my-2 text-lg"><a class="no-underline hover:underline" href="{{ post.url | prepend: site.url }}">{{post.title}}</a> <span class='text-grey text-sm'>on {{ post.date | date: '%b %d, %Y' }}</span></li>
+    <li class="my-2 text-lg"><a class="no-underline hover:underline" href="{{ post.url | prepend: site.url }}">{{post.title}}</a> <span class='text-gray-400 text-sm'>on {{ post.date | date: '%b %d, %Y' }}</span></li>
   {%- endfor -%}
   </ul>
 </div>
