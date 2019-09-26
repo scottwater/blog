75ac7d5dc5c2ea997c400b925c1969d348329c4c
Scott Watermasysk <scottwater@gmail.com>
2019-09-26 14:47:18 -0400

    Without "Apple Color Emoji"

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,34 +1,44 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const filteredFonts = defaultTheme.fontFamily.sans.filter(
+  font => font !== '"Apple Color Emoji"'
+);
+console.log({ filteredFonts });
 module.exports = {
   theme: {
-    maxWidth: { //based on 0.7
-      'xs': '20rem',
-      'sm': '30rem',
-      'md': '40rem',
-      'lg': '50rem',
-      'xl': '60rem',
-      '2xl': '70rem',
-      '3xl': '80rem',
-      '4xl': '90rem',
-      '5xl': '100rem',
-      'full': '100%',
+    maxWidth: {
+      //based on 0.7
+      xs: "20rem",
+      sm: "30rem",
+      md: "40rem",
+      lg: "50rem",
+      xl: "60rem",
+      "2xl": "70rem",
+      "3xl": "80rem",
+      "4xl": "90rem",
+      "5xl": "100rem",
+      full: "100%"
     },
     fontFamily: {
-      'headlines' : [
-        'Rubik', 
-        'Constantia',
-        'Lucida Bright',
-        'Lucidabright',
-        'Lucida Serif',
-        'Lucida',
-        'DejaVu Serif',
-        'Bitstream Vera Serif',
-        'Liberation Serif',
-        'Georgia',
-        'serif',
-      ],
+      headlines: [
+        "Rubik",
+        "Constantia",
+        "Lucida Bright",
+        "Lucidabright",
+        "Lucida Serif",
+        "Lucida",
+        "DejaVu Serif",
+        "Bitstream Vera Serif",
+        "Liberation Serif",
+        "Georgia",
+        "serif"
+      ]
     },
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: [...filteredFonts]
+      }
+    }
   },
   variants: {},
   plugins: []
-}
+};
